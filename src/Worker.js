b0447a0625f7234bb2b06790e90ac5f2a911b104
souha47 <s.hocini@esi-sba.dz>
2023-06-05 00:07:37 +0100

    responsiviti

--- src/Worker.js
+++ src/Worker.js
@@ -145,6 +145,7 @@ function showdoc(){
     doclist.style.display = 'flex';
     doclist.style.justifyContent = 'center';
     doclist.style.alignItems = 'center';
+    
 
   const starCountRef = databaseURL(database,"user/");
   onValue(starCountRef, (snapshot) => {
@@ -639,7 +640,139 @@ uploadBytes(imageRef, photo.files[0])
 
 
 // initialize de wilaya algerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr--------------------------------
+let searchBar = document.getElementById('searchBar'); 
+function showdash (){
+  console.log("true");
+  searchBar.style.display='none';
+}
+//dashboard contenet
+let dashb = document.getElementById('dashb');
 
+function showdassh (){
+  console.log("true");
+  searchBar.style.display='none';
+  dashb.style.display= 'block';
+  doclist.style.display='none';
+  nurslist.style.display='none';
+  pharmlist.style.display='none';
+  receplist.style.display='none';
+  AddForm.style.display='none';
+}
+
+
+// ---------- CHARTS ----------
+
+// BAR CHART
+var barChartOptions = {
+  series: [{
+    data: [15, 20, 5, 3],
+    name: "Products",
+  }],
+  chart: {
+    type: "bar",
+    background: "transparent",
+    height: 350,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: [
+    "#227ECC",
+    "#0F9B96",
+    "#376293",
+    "#0a1018",
+    
+  ],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: "40%",
+    }
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  fill: {
+    opacity: 1,
+  },
+  grid: {
+    borderColor: "#55596e",
+    yaxis: {
+      lines: {
+        show: true,
+      },
+    },
+    xaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+  legend: {
+    labels: {
+      colors: "#f5f7ff",
+    },
+    show: true,
+    position: "top",
+  },
+  stroke: {
+    colors: ["transparent"],
+    show: true,
+    width: 2
+  },
+  tooltip: {
+    shared: true,
+    intersect: false,
+    theme: "dark",
+  },
+  xaxis: {
+    categories: ["Doctors", "Nurses", "Pharmacies", "Receptionists" ],
+    title: {
+      style: {
+        color: "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      show: true,
+      color: "#55596e",
+    },
+    axisTicks: {
+      show: true,
+      color: "#55596e",
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  },
+  yaxis: {
+    title: {
+      text: "Count",
+      style: {
+        color:  "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      color: "#55596e",
+      show: true,
+    },
+    axisTicks: {
+      color: "#55596e",
+      show: true,
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  }
+};
+
+var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+barChart.render();
 
 
 
